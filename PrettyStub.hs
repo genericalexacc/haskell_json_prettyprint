ff5f786a60e5f92e89d656c55ad5fabea14c9353
Ubuntu <ubuntu@ip-172-31-52-70.ec2.internal>
2019-09-27 17:55:12 +0000

    init

---
+++ PrettyStub.hs
@@ -0,0 +1,25 @@
+import SimpleJSON
+
+data Doc = ToBeDefined
+    deriving (Show)
+
+string :: String -> Doc
+string str = undefined
+
+text :: String -> Doc
+text str = undefined
+
+double :: Double -> Doc
+double num = undefined
+
+(<>) :: Doc -> Doc -> Doc
+a <> b = undefined
+
+char :: Char -> Doc
+char c = undefined
+
+hcat :: [Doc] -> Doc
+hcat xs = undefined
+
+fsep :: [Doc] -> Doc
+fsep xs = undefined
---
+++ dist/build/autogen/Paths_mypretty.hs
@@ -0,0 +1,35 @@
+module Paths_mypretty (
+    version,
+    getBinDir, getLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+catchIO = Exception.catch
+
+version :: Version
+version = Version [0,1] []
+bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/usr/local/bin"
+libdir     = "/usr/local/lib/x86_64-linux-ghc-7.10.3/mypretty-0.1-AvOxFqDtoni5T7Ig2ghO6u"
+datadir    = "/usr/local/share/x86_64-linux-ghc-7.10.3/mypretty-0.1"
+libexecdir = "/usr/local/libexec"
+sysconfdir = "/usr/local/etc"
+
+getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "mypretty_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "mypretty_libdir") (\_ -> return libdir)
+getDataDir = catchIO (getEnv "mypretty_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "mypretty_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "mypretty_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
